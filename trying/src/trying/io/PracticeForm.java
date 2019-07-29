a1fa2b57215aac2391970bf2a364860cec816ae2
majd <majd1999ziad@gmail.com>
2019-07-30 01:24:53 +0300

    created object from basic class to handle the process

--- trying/src/trying/io/PracticeForm.java
+++ trying/src/trying/io/PracticeForm.java
@@ -14,6 +14,8 @@ public class PracticeForm extends javax.swing.JFrame {
     /**
      * Creates new form PracticeForm
      */
+    // created object from basic class to handle the process
+    BasicClass basicClass=new BasicClass();
     public PracticeForm() {
         initComponents();
     }
