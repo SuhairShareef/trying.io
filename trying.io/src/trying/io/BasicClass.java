9ede1b8d14ee0927dc64488cfd2b11e469dea923
majd <majd1999ziad@gmail.com>
2019-07-29 23:51:55 +0300

    added documentation for the class

--- trying.io/src/trying/io/BasicClass.java
+++ trying.io/src/trying/io/BasicClass.java
@@ -6,7 +6,9 @@
 package trying.io;
 
 /**
- *
+ *This class for dealing with the typed characters  written by the user and list of errors from the GUI application
+ * there is a a method for initialize the member variables , set and get the value of each member variable 
+ * method for counts number of words per minute  and  listing the errors .
  * @author majd1
  */
 public class BasicClass {
