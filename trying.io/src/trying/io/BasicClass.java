15f2044f52acb5afb792d36377ec3cee7a44467c
majd <majd1999ziad@gmail.com>
2019-07-29 23:53:29 +0300

    added member variable for error list

--- trying.io/src/trying/io/BasicClass.java
+++ trying.io/src/trying/io/BasicClass.java
@@ -5,6 +5,8 @@
  */
 package trying.io;
 
+import java.util.HashMap;
+
 /**
  *This class for dealing with the typed characters  written by the user and list of errors from the GUI application
  * there is a a method for initialize the member variables , set and get the value of each member variable 
@@ -13,4 +15,6 @@ package trying.io;
  */
 public class BasicClass {
     
+    // member variable that store uncorrected character  as "key" and number of errors for each as "value"
+    private HashMap<Character,Integer> charErrors;
 }
